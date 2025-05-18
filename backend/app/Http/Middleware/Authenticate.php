ffe9e4efd6cc7ff0170ad601b72eb576aa84c568
npaveza <n.paveza@duocuc.cl>
2025-05-18 11:44:11 -0400

    rel_1.0.4.1 - Correcciones para JWT

--- backend/app/Http/Middleware/Authenticate.php
+++ backend/app/Http/Middleware/Authenticate.php
@@ -10,8 +10,18 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    #protected function redirectTo(Request $request): ?string
+    #{
+    #    return $request->expectsJson() ? null : route('login');
+    #}
+
+    protected function redirectTo($request)
     {
-        return $request->expectsJson() ? null : route('login');
+        abort(response()->json([
+            'message' => 'No autenticado.'
+        ], 401));
     }
+
+
+
 }
